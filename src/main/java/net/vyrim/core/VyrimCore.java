0ea1e9a00fbca8d4c626dec519d23dbb78342a3f
Diego <diegolopezgundin@gmail.com>
2026-08-26 14:38:26 +0200

    Database

--- src/main/java/net/vyrim/core/VyrimCore.java
+++ src/main/java/net/vyrim/core/VyrimCore.java
@@ -1,16 +1,29 @@
 package net.vyrim.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import net.vyrim.core.storage.StorageManager;
 
 public final class VyrimCore extends JavaPlugin {
+    private static VyrimCore instance;
+    private StorageManager storageManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        saveDefaultConfig();
+
+        this.storageManager = new StorageManager(this);
+        storageManager.connect();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (storageManager != null) {
+            storageManager.close();
+        }
+    }
+
+    public StorageManager storage() {
+        return storageManager;
     }
 }
